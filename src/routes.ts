d2796ac137cd62a716d8530b41530af221701f69
Rafael Mariano da Silva <rafaelmarianodasilva@MacBook-Air-de-Rafael.local>
2024-05-31 10:44:35 -0300

    feat: adiciona rota para busca por id

--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,7 @@ import UpdateUserService from './services/update-user.service';
 import GetAllUserService from './services/get-all-user.service';
 import RemoveUserService from './services/remove-user.service';
 import ValidateUserService from './services/validate-user.service';
+import GetUserByIDService from './services/get-user-by-id.service';
 
 const router = Router();
 const userRepository = new UserRepository();
@@ -14,6 +15,7 @@ const updateUserService = new UpdateUserService(userRepository);
 const getAllUserService = new GetAllUserService(userRepository);
 const removeUserService = new RemoveUserService(userRepository);
 const validateUserService = new ValidateUserService(userRepository);
+const getUserByIDService = new GetUserByIDService(userRepository);
 
 const userController = new UserController(
   addUserService,
@@ -21,9 +23,11 @@ const userController = new UserController(
   getAllUserService,
   removeUserService,
   validateUserService,
+  getUserByIDService,
 );
 
 router.get('/', userController.getAll);
+router.get('/:id', userController.getById);
 router.post('/', userController.addUser);
 router.patch('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
